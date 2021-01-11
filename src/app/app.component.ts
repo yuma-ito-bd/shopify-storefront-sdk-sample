499fa4050bc5df5560d968dc481c019e9e809166
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2021-01-11 22:34:21 +0900

    カート機能を追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,19 @@ export class AppComponent implements OnInit {
         this.products.forEach((product) => {
             console.log(product);
         });
+
+        await this.shopify.createCheckout();
+
+        const redTshirtsSmallId = this.products[0].variants[0].id;
+        const redTshirtsMediumId = this.products[0].variants[1].id;
+        const redTshirtsLargeId = this.products[0].variants[2].id;
+        const whiteTshirtsId = this.products[1].variants[0].id;
+
+        // FIXME: 連続で実行するのはパフォーマンスが悪い
+        await this.shopify.addLineItem(whiteTshirtsId, 1);
+        await this.shopify.addLineItem(redTshirtsSmallId, 1);
+        await this.shopify.addLineItem(redTshirtsMediumId, 2);
+        await this.shopify.addLineItem(redTshirtsLargeId, 3);
+        console.log(await this.shopify.getCheckoutUrl());
     }
 }
--- src/app/services/shopify-sdk-wrapper.service.ts
+++ src/app/services/shopify-sdk-wrapper.service.ts
@@ -7,6 +7,7 @@ import { environment } from 'src/environments/environment';
 })
 export class ShopifySdkWrapperService {
     private client: ShopifyBuy.Client;
+    private checkoutId?: string;
 
     constructor() {
         const { shopifyDomain, storefrontAccessToken } = environment;
@@ -21,5 +22,42 @@ export class ShopifySdkWrapperService {
      */
     public fetchAllProducts(): Promise<ShopifyBuy.Product[]> {
         return this.client.product.fetchAll();
+    }
+
+    /**
+     * createCheckout
+     */
+    public async createCheckout(): Promise<void> {
+        const cart = await this.client.checkout.create();
+        this.checkoutId = String(cart.id);
+        console.log(this.checkoutId);
+    }
+
+    /**
+     * addLineItem
+     */
+    public async addLineItem(
+        variantId: string | number,
+        quantity: number
+    ): Promise<void> {
+        const checkoutId = this.checkoutId;
+        if (checkoutId == null) {
+            throw new Error('invalid value');
+        }
+
+        const item: ShopifyBuy.LineItemToAdd = { variantId, quantity };
+        await this.client.checkout.addLineItems(checkoutId, [item]);
+        console.log(await this.client.checkout.fetch(String(checkoutId)));
+    }
+
+    public async getCheckoutUrl(): Promise<string | undefined> {
+        const id = this.checkoutId;
+        if (id == null) {
+            throw new Error('invalid value');
+        }
+
+        const cart = await this.client.checkout.fetch(id);
+        // webUrlが型定義ファイルに存在しないためanyでコンパイルエラーを回避する
+        return (cart as any).webUrl;
     }
 }
