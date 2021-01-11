21732065bd5994495de41e7bc9dba38cda633417
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2021-01-11 21:24:11 +0900

    商品情報の取得・表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShopifySdkWrapperService } from './services/shopify-sdk-wrapper.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'shopify-storefront-sdk-sample';
+export class AppComponent implements OnInit {
+    title = 'shopify-storefront-sdk-sample';
+    products!: ShopifyBuy.Product[];
+
+    constructor(private shopify: ShopifySdkWrapperService) {}
+
+    async ngOnInit(): Promise<void> {
+        this.products = await this.shopify.fetchAllProducts();
+        this.products.forEach((product) => {
+            console.log(product);
+        });
+    }
 }
